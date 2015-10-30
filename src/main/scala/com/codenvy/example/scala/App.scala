75784632978b814dda5113d7f7b96f2633e40dc7
Anonymous <sheik5azmal@gmail.com>
2015-10-30 05:08:08 +0000

    u

--- src/main/scala/com/codenvy/example/scala/App.scala
+++ src/main/scala/com/codenvy/example/scala/App.scala
@@ -8,7 +8,7 @@ object SparkMeApp {
     val sc = new SparkContext(conf)
 
     val fileName = args(0)
-    val lines = sc.textFile(sample.txt).cache()
+    val lines = sc.textFile("sample.txt").cache()
 
     val c = lines.count()
     println(s"There are $c lines in $fileName")
